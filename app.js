bf02af6ba28232bf616571638db9997ab97afe62
cameron <mrmuffinsworth8@gmail.com>
2026-02-15 16:05:04 -0500

    Handles POST requests

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+// middlware
+app.use(express.json());
+
 /*app.get('/', (request, response) => {
   response.status(200).json(
     {
@@ -25,6 +28,23 @@ app.get('/api/v1/tours', (req, res) => {
     }
   })
 });
+
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+  const newID = tours[tours.length - 1].id + 1;
+  //Object.assign will merge two JS objects
+  const newTour = Object.assign({ id: newID }, req.body);
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    if (err) return console.error(err.message);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    })
+  });
+})
 
 const port = 3000;
 app.listen(port, () => {
