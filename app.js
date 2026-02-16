f49579253ccb826dfd26e3a1195c329fe7551b26
cameron <mrmuffinsworth8@gmail.com>
2026-02-15 21:32:26 -0500

    refactoring the server into seperate components to have isolated modules

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 // middlware - order is important
 app.use(morgan('dev'));
@@ -20,97 +22,11 @@ app.use((req, res, next) => {
   next();
 });
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-const getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours: tours
-    }
-  })
-};
-const getTour = (req, res) => {
-  const id = req.params.id * 1;
-  if (id > tours.length) return res.status(404).json({ status: 'failed', message: 'Tour does not exist' });
-  const tour = tours.find(el => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    tour: tour
-  })
-};
+// creates a sub application = mouting the router
+app.use('/api/v1/tours', tourRouter);
 
-const createTour = (req, res) => {
-  //console.log(req.body);
-  const newID = tours[tours.length - 1].id + 1;
-  //Object.assign will merge two JS objects
-  const newTour = Object.assign({ id: newID }, req.body);
-  tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    if (err) return console.error(err.message);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour
-      }
-    })
-  });
-}
-
-const updateTour = (req, res) => {
-  if (req.params.id > tours.length) return res.status(404).json({ status: 'failed', message: 'Invalid ID' });
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated Tour Here>'
-    }
-  })
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id > tours.length) return res.status(404).json({ status: 'failed', message: 'Invalid ID' });
-  res.status(204).json({
-    status: 'success',
-    data: null
-  })
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined'
-  })
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined'
-  })
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined'
-  })
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined'
-  })
-};
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined'
-  })
-};
+app.use('/api/v1/users', userRouter);
 
 //app.get('/api/v1/tours', getAllTours);
 //app.get('/api/v1/tours/:id', getTour);
@@ -118,28 +34,4 @@ const deleteUser = (req, res) => {
 //app.patch('/api/v1/tours/:id', updateTour)
 //app.delete('/api/v1/tours/:id', deleteTour)
 
-const tourRouter = express.Router();
-
-// creates a sub application = mouting the router
-app.use('/api/v1/tours', tourRouter);
-
-tourRouter.route('/')
-  .get(getAllTours)
-  .post(createTour);
-
-tourRouter.route('/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
-
-const userRouter = express.Router();
-
-app.use('/api/v1/users', userRouter);
-
-userRouter.route('/').get(getAllUsers).post(createUser);
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port: ${port}`);
-})
+module.exports = app;
