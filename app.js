9e2585dcdd8d910d72f7f849484e9de6d4de1712
cameron <mrmuffinsworth8@gmail.com>
2026-02-15 19:29:48 -0500

    creating our own middleware

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-// middlware
+// middlware - order is important
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  //without next the server will hang because it blocks
+  next();
+})
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours: tours
