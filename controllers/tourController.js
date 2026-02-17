9b12e1856a3e0c78e27fc329d9bc083f52cce514
cameron <mrmuffinsworth8@gmail.com>
2026-02-16 20:26:25 -0500

    chaining multiple middleware functions

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -8,6 +8,14 @@ exports.checkID = (req, res, next, val) => {
   next();
 }
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) return res.status(404).json({
+    status: 'fail',
+    message: 'Does not contain name or price'
+  })
+  next();
+}
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -35,7 +43,7 @@ exports.createTour = (req, res) => {
   //Object.assign will merge two JS objects
   const newTour = Object.assign({ id: newID }, req.body);
   tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+  fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
     if (err) return console.error(err.message);
     res.status(201).json({
       status: 'success',
