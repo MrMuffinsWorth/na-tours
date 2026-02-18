ee7eec422c398ba5b37d85d103b257ffe750aee4
cameron <mrmuffinsworth8@gmail.com>
2026-02-18 16:35:47 -0500

    making the API better: sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -16,8 +16,14 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
     console.log(JSON.parse(queryStr));
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
     // { difficulty: 'easy', duration: {$gte: 5 }} - mongo filter object
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query.sort(sortBy);
+      // sort('price ratingAverage')
+    }
 
     const tours = await query;
     res.status(200).json({
