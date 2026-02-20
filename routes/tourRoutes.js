56687a5c6852d9f986b47910d51d6474639c5a4f
cameron <mrmuffinsworth8@gmail.com>
2026-02-19 20:44:11 -0500

    aggregation pipeline: unwinding and projecting

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,8 @@ router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.ge
 
 router.route('/tour-stats').get(tourController.getTourStats);
 
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
 router.route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);
