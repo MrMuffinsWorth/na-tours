dcfab74a2045167f5e34f0fe7b1772e94c425a31
cameron <mrmuffinsworth8@gmail.com>
2026-02-21 13:32:43 -0500

    modeling users

---
+++ models/userModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide your name.']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email.'],
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: [validator.isEmail, 'A valid email is required.']
+    }
+  },
+  photo: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password.'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password.'],
+    validate: {
+      validator: function(passwordConfirm) {
+        return this.password === passwordConfirm
+      },
+      message: 'Passwords must match.'
+    }
+  }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
