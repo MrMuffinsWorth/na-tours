49fc49257d5234036576a7354e4735b433037c8d
cameron <mrmuffinsworth8@gmail.com>
2026-02-21 08:42:56 -0500

    handling unhandled routes

--- app.js
+++ app.js
@@ -16,22 +16,24 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  //without next the server will hang because it blocks
-  next();
-})
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
 
 // creates a sub application = mouting the router
 app.use('/api/v1/tours', tourRouter);
 
 app.use('/api/v1/users', userRouter);
 
+// catches all requests not handled by our routes (does not use the old regex pattern '*' to match)
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server.`
+  })
+  next();
+})
+// runs for all http methods
 //app.get('/api/v1/tours', getAllTours);
 //app.get('/api/v1/tours/:id', getTour);
 //app.post('/api/v1/tours', createTour)
