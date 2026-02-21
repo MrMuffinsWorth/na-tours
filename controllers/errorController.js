26dc3662e52b5328e446f24bd6947b936bacc404
cameron <mrmuffinsworth8@gmail.com>
2026-02-21 10:11:53 -0500

    errors during development vs production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,35 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  })
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    })
+  } else {
+    console.error('ERROR: ', err);
+    res.status(err.statusCode).json({
+      status: 'error',
+      message: 'Something went very wrong.'
+    })
+  }
+}
 
 // express knows that this is an error handling middleware
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error'
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  })
+
+  if (process.end.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.end.NODE_ENV === 'development') {
+    sendErrorProd(err, res);
+  }
 };
