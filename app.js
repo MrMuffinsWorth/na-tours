dc979cefa62607a31e60cdf9bd769349c9d00656
cameron <mrmuffinsworth8@gmail.com>
2026-02-21 09:08:01 -0500

    better errors and refactoring

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
+const AppError = require('./utils/appError');
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const globalErrorHandler = require('./controllers/errorController')
 
 // middlware - order is important
 if (process.env.NODE_ENV === 'development') {
@@ -27,20 +29,9 @@ app.use('/api/v1/users', userRouter);
 
 // catches all requests not handled by our routes (does not use the old regex pattern '*' to match)
 app.use((req, res, next) => {
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err); // if next has a function argument it knows there is an error and sends to the global error middleware
+  next(new AppError(`Can't find ${req.originalUrl} on this server`), 404); // if next has a function argument it knows there is an error and sends to the global error middleware
 })
 
-// express knows that this is an error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error'
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  })
-})
+app.use(globalErrorHandler)
 
 module.exports = app;
