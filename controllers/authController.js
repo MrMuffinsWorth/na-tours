70e5148a4ed5266b87877fb646b953f410cfaa25
cameron <mrmuffinsworth8@gmail.com>
2026-02-22 10:36:38 -0500

    password reset functionality: reset token

--- controllers/authController.js
+++ controllers/authController.js
@@ -87,3 +87,20 @@ exports.restrictTo = (...roles) => {
     next();
   }
 }
+
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  //get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email })
+  if (!user) return next(new AppError('There is no user with that email address.', 404));
+  //generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+  //send it to user's email
+  res.status(200).json({
+    status: 'success',
+    message: 'Token sent to email!',
+    resetToken
+  });
+});
+exports.resetPassword = (req, res, next) => { }
--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs'); // bcrypt native may be better than bcryptjs
+const crypto = require('crypto');
 const catchAsync = require('../utils/catchAsync');
 
 const userSchema = new mongoose.Schema({
@@ -40,6 +41,8 @@ const userSchema = new mongoose.Schema({
     }
   },
   passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date
 })
 
 userSchema.pre('save', async function() {
@@ -62,6 +65,15 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
   // false means not changed
   return false;
 }
+
+userSchema.methods.createPasswordResetToken = function() {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+  this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+  console.log({ resetToken }, this.passwordResetToken)
+  return resetToken;
+}
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
