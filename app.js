008eaf9efd4b30892b644626a50e5f530552d8c0
cameron <mrmuffinsworth8@gmail.com>
2026-02-24 07:02:35 -0500

    setting security HHTP headers

--- app.js
+++ app.js
@@ -3,30 +3,38 @@ const AppError = require('./utils/appError');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const globalErrorHandler = require('./controllers/errorController')
 
-// middlware - order is important
+// Set security HTTP headers
+app.use(helmet());
+
+// Development loggin middlware - order is important
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests on API from same IP
 const limiter = rateLimit({
   max: 100,  // 100 requests
   windowMs: 60 * 60 * 1000, // 1 hour
   message: 'Too many requests from this IP, please try again in an hour!'
 });
+app.use('/api', limiter);
 
-app.use('/api', limiter); // puts rate limit on all of the api routes
 
 app.set('query parser', 'extended'); // required for advanced mongo url param filtering
 
-app.use(express.json());
+// Body, parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' })); // allows body to only be 10kb
 
+// Serving static files
 app.use(express.static(`${__dirname}/public`))
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
