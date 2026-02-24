1828b3ba59af8a2c501c5b4474c02c765f5d57a9
cameron <mrmuffinsworth8@gmail.com>
2026-02-24 07:17:42 -0500

    preventing parameter pollution

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -38,6 +39,11 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS attacks 
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp({
+  whitelist: ['duration', 'ratingsQuantity', 'ratingsAverage', 'maxGroupSize', 'difficulty', 'price']
+}));
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`))
