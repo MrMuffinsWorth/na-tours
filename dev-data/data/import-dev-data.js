fadb34ffe1fb015bc33062b40a1769fe1097131c
cameron <mrmuffinsworth8@gmail.com>
2026-02-28 11:39:56 -0500

    modeling locations (Geospatial data)

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -7,7 +7,7 @@ dotenv.config({ path: './.env' });
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 mongoose.connect(DB).then(() => console.log('DB connection successful'));
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 const importData = async () => {
   try {
