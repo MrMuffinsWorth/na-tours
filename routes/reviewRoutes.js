06981f856b79b3aca4630d6428c410ca34d63b7b
cameron <mrmuffinsworth8@gmail.com>
2026-03-04 05:38:39 -0500

    adding missing authentication and authorization

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,13 +4,15 @@ const express = require('express');
 
 const router = express.Router({ mergeParams: true }); // mergeParams will grab the params from the tour router if the request goes through there first
 
+router.use(authController.protect);
+
 router.route('/')
   .get(reviewController.getAllReviews)
-  .post(authController.protect, authController.restrictTo('user', 'admin'), reviewController.setTourUserIds, reviewController.createReview)
+  .post(authController.protect, authController.restrictTo('user'), reviewController.setTourUserIds, reviewController.createReview)
 
 router.route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview)
+  .patch(authController.restrictTo('admin', 'user'), reviewController.updateReview)
+  .delete(authController.restrictTo('admin', 'user'), reviewController.deleteReview)
 
 module.exports = router
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,12 +10,21 @@ router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch('/updatePassword', authController.protect, authController.updatePassword);
+
+router.use(authController.protect);
+
+router.route('/:tourId/reviews')
+  .post(authController.restrictTo('user'), reviewController.createReview)
 
 router.get('/me', authController.protect, userController.getMe, userController.getUser)
 
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updatePassword', authController.protect, authController.updatePassword);
+
+router.patch('/updateMe', userController.updateMe);
+
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router.route('/')
   .get(userController.getAllUsers)
@@ -25,9 +34,5 @@ router.route('/:id')
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
-
-
-router.route('/:tourId/reviews')
-  .post(authController.protect, authController.restrictTo('user'), reviewController.createReview)
 
 module.exports = router;
