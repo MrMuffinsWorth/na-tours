e27fd686d719b741ab43ca0d23deddc39f221421
cameron <mrmuffinsworth8@gmail.com>
2026-03-14 12:00:47 -0400

    first steps with pug templates

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const { clean } = require('xss-clean/lib/xss');
 const hpp = require('hpp');
-const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const globalErrorHandler = require('./controllers/errorController')
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, './views'));
 
 // Set security HTTP headers
 app.use(helmet());
@@ -76,13 +80,20 @@ app.use(hpp({
 }));
 
 // Serving static files
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas'
+  });
+})
 
 // creates a sub application = mouting the router
 app.use('/api/v1/tours', tourRouter);
