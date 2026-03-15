ad9a888f72d62d17b976ac41befcc5cbc2a205ca
cameron <mrmuffinsworth8@gmail.com>
2026-03-15 09:35:05 -0400

    extending our base template with blocks

--- app.js
+++ app.js
@@ -95,6 +95,18 @@ app.get('/', (req, res) => {
   });
 })
 
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours'
+  })
+})
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour'
+  })
+})
+
 // creates a sub application = mouting the router
 app.use('/api/v1/tours', tourRouter);
 
